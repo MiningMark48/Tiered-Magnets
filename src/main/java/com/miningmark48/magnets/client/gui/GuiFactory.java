106412ffae77902915b1b8dcdafc59e27820700a
Mark <miningmark48@gmail.com>
2019-07-04 16:58:13 -0400

    Config gui is back, added more names and descriptions to configs

--- src/main/java/com/miningmark48/magnets/client/gui/GuiFactory.java
+++ src/main/java/com/miningmark48/magnets/client/gui/GuiFactory.java
@@ -1,25 +1,31 @@
 package com.miningmark48.magnets.client.gui;
 
-public class GuiFactory /* implements IModGuiFactory */ {
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.IModGuiFactory;
 
-//    @Override
-//    public void initialize(Minecraft minecraftInstance) {
-//
-//    }
-//
-//    @Override
-//    public boolean hasConfigGui() {
-//        return true;
-//    }
-//
-//    @Override
-//    public GuiScreen createConfigGui(GuiScreen parentScreen) {
-//        return null; //new GuiModConfig(parentScreen);
-//    }
-//
-//    @Override
-//    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
-//        return null;
-//    }
+import java.util.Set;
+
+public class GuiFactory implements IModGuiFactory {
+
+    @Override
+    public void initialize(Minecraft minecraftInstance) {
+
+    }
+
+    @Override
+    public boolean hasConfigGui() {
+        return true;
+    }
+
+    @Override
+    public GuiScreen createConfigGui(GuiScreen parentScreen) {
+        return new GuiModConfig(parentScreen);
+    }
+
+    @Override
+    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+        return null;
+    }
 
 }
--- src/main/java/com/miningmark48/magnets/client/gui/GuiModConfig.java
+++ src/main/java/com/miningmark48/magnets/client/gui/GuiModConfig.java
@@ -1,5 +1,12 @@
 package com.miningmark48.magnets.client.gui;
 
-public class GuiModConfig {
+import com.miningmark48.magnets.reference.Reference;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.config.GuiConfig;
 
+public class GuiModConfig extends GuiConfig {
+
+    public GuiModConfig(GuiScreen parentScreen) {
+        super(parentScreen, Reference.MOD_ID, Reference.MOD_NAME);
+    }
 }
