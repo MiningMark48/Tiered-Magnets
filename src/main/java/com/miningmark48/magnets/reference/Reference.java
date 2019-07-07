c6553f42ceabbae5b186dc6f72afbb40f2635455
Mark <miningmark48@gmail.com>
2019-07-07 02:09:30 -0400

    Dev build 3

--- src/main/java/com/miningmark48/magnets/reference/Reference.java
+++ src/main/java/com/miningmark48/magnets/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "magnets";
     public static final String MOD_NAME = "Magnets";
-    public static final String MOD_VERSION = "0.0.1";
+    public static final String MOD_VERSION = "0.0.1-dev3";
     public static final String GAME_VERSION = "1.12.2";
     public static final String VERSION = GAME_VERSION + "-" + MOD_VERSION;
     public static final String MOD_DEPENDENCIES = "";
