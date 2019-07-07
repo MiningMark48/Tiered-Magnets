12e6a68629a827f8b116e828404019997b834a8e
Mark <miningmark48@gmail.com>
2019-07-07 13:16:55 -0400

    TESR won't crash server anymore

--- src/main/java/com/miningmark48/magnets/init/ModTileEntities.java
+++ src/main/java/com/miningmark48/magnets/init/ModTileEntities.java
@@ -1,7 +1,11 @@
 package com.miningmark48.magnets.init;
 
 import com.miningmark48.magnets.tileentity.TileEntityMagneticInsulator;
+import com.miningmark48.magnets.tileentity.renderer.RendererMagneticInsulator;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ModTileEntities {
 
@@ -9,8 +13,9 @@ public class ModTileEntities {
         GameRegistry.registerTileEntity(TileEntityMagneticInsulator.class, "magnetic_insulator");
     }
 
+    @SideOnly(Side.CLIENT)
     public static void bindTileRenders() {
-//        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityMagneticInsulator.class, new RendererMagneticInsulator());
+        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityMagneticInsulator.class, new RendererMagneticInsulator());
     }
 
 }
