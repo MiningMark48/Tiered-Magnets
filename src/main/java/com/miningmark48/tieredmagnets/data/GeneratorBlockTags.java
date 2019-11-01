78de064f0f3ca8981a4865fe819d4f17ff1c0f48
Mark <miningmark48@gmail.com>
2019-11-01 00:04:14 -0400

    Data Generators: Tags

---
+++ src/main/java/com/miningmark48/tieredmagnets/data/GeneratorBlockTags.java
@@ -0,0 +1,60 @@
+package com.miningmark48.tieredmagnets.data;
+
+import net.minecraft.block.Block;
+import net.minecraft.data.BlockTagsProvider;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.tags.Tag;
+
+import java.util.Arrays;
+import java.util.function.IntFunction;
+import java.util.function.Supplier;
+
+public class GeneratorBlockTags extends BlockTagsProvider {
+
+    public GeneratorBlockTags(DataGenerator generatorIn) {
+        super(generatorIn);
+    }
+
+    @Override
+    protected void registerTags() {
+
+    }
+
+    @SafeVarargs
+    private final <T> T[] resolveAll(IntFunction<T[]> creator, Supplier<? extends T>... suppliers) {
+        return Arrays.stream(suppliers).map(Supplier::get).toArray(creator);
+    }
+
+    @SafeVarargs
+    private final void createTag(Tag<Block> tag, Supplier<? extends Block>... blocks) {
+        getBuilder(tag).add(resolveAll(Block[]::new, blocks));
+    }
+
+    @SafeVarargs
+    private final void appendToTag(Tag<Block> tag, Tag<Block>... toAppend) {
+        getBuilder(tag).add(toAppend);
+    }
+
+    @SafeVarargs
+    private final void extendTag(Tag<Block> tag, Tag<Block> toExtend, Supplier<? extends Block>... blocks) {
+        getBuilder(tag).add(toExtend).add(resolveAll(Block[]::new, blocks));
+    }
+
+    @SafeVarargs
+    private final void createAndAppend(Tag<Block> tag, Tag<Block> to, Supplier<? extends Block>... blocks) {
+        createTag(tag, blocks);
+        appendToTag(to, tag);
+    }
+
+    @SafeVarargs
+    private final void extendAndAppend(Tag<Block> tag, Tag<Block> toExtend, Tag<Block> to, Supplier<? extends Block>... blocks) {
+        extendTag(tag, toExtend, blocks);
+        appendToTag(to, tag);
+    }
+
+    @Override
+    public String getName() {
+        return "Tiered Magnets Block Tags";
+    }
+
+}
---
+++ src/main/java/com/miningmark48/tieredmagnets/data/GeneratorItemTags.java
@@ -0,0 +1,39 @@
+package com.miningmark48.tieredmagnets.data;
+
+import com.miningmark48.tieredmagnets.init.ModItems;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.ItemTagsProvider;
+import net.minecraft.item.Item;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.IItemProvider;
+
+import java.util.Arrays;
+import java.util.function.Supplier;
+
+public class GeneratorItemTags extends ItemTagsProvider {
+
+    public GeneratorItemTags(DataGenerator generatorIn) {
+        super(generatorIn);
+    }
+
+    @Override
+    protected void registerTags() {
+        addItemsToTag(ModTags.Items.MAGNETITE, ModItems.MAGNETITE);
+    }
+
+    @SafeVarargs
+    private final void addItemsToTag(Tag<Item> tag, Supplier<? extends IItemProvider>... items) {
+        getBuilder(tag).add(Arrays.stream(items).map(Supplier::get).map(IItemProvider::asItem).toArray(Item[]::new));
+    }
+
+    @SafeVarargs
+    private final void appendToTag(Tag<Item> tag, Tag<Item>... toAppend) {
+        getBuilder(tag).add(toAppend);
+    }
+
+    @Override
+    public String getName() {
+        return "Tiered Magnets Item Tags";
+    }
+
+}
